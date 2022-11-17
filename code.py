5c3c0f3607f40607c1b3b17a001e88fd17561a7d
myseneca-153799200 <tcjayswal@myseneca.ca>
2022-11-17 11:23:50 -0800

    defining the loops

--- code.py
+++ code.py
@@ -2,3 +2,17 @@
 #Date Created = 2022/11/17
 
 import random
+
+options = ("rock", "paper", "scissors")
+gameruns = True
+
+while gameruns:
+
+    user = None
+    computer = random.choice(options)
+
+    while user not in options:
+        user = input("Enter a choice (rock, paper, scissors): ")
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
